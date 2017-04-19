adcb0b861d4cdb5a4659f80b6651b09f0b79c792
Vladimir Kikhtenko <kva911@gmail.com>
2017-04-19 22:21:35 +0700

    components, descriptors and translations

--- cards.go
+++ cards.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"errors"
+	"flag"
 	"html/template"
 	"io"
 	"log"
@@ -35,6 +36,7 @@ var translation = map[string]string{
 	"3 full rounds": "3 раунда",
 	"6 rounds":      "6 раундов",
 
+	"1 min.":                "1 минута",
 	"1 minute":              "1 минута",
 	"Casting time 1 minute": "1 минута",
 	"1 minute per page":     "1 минута на страницу",
@@ -65,25 +67,76 @@ var translation = map[string]string{
 	"see text": "см. текст",
 
 	//Duration
-	"instant":       "мгновенное",
-	"instantaneous": "мгновенное",
-	"Instantaneous": "мгновенное",
+	"instant":                    "мгновенное",
+	"instantaneous":              "мгновенное",
+	"Instantaneous":              "мгновенное",
+	"instantaneous; see text":    "мгновенное, см. текст",
+	"permanent":                  "постоянное",
+	"permanent (D)":              "постоянное (до отмены)",
+	"permanent until discharged": "постоянное (до отмены)",
+	"concentration":              "концентрация",
+	"Concentration + 2 rounds":   "концентрация + 2 раунда",
+
+	"concentration, up to 1 minute/level":        "концентрация, 1 мин./ур.",
+	"concentration, up to 1 minute/ level":       "концентрация, 1 мин./ур.",
+	"concentration, up to 1 min./level":          "концентрация, 1 мин./ур.",
+	"concentration, up to 10 min./level":         "концентрация, 10 мин./ур.",
+	"concentration, up to 10 min./ level":        "концентрация, 10 мин./ур.",
+	"1 round/level":                              "1 раунд/уровень",
+	"1 round + 1 round per three levels":         "1 раунд + 1 раунд/3 ур.",
+	"3 rounds/level":                             "3 раунда/уровень",
+	"1 minute or until discharged":               "1 минута или до отмены",
+	"1 min./level":                               "1 минута/уровень",
+	"1 min./level (D)":                           "1 мин./ур. или до отмены",
+	"10 min./level":                              "10 минут/уровень",
+	"30 minutes or until discharged":             "30 минут или до отмены",
+	"1d4 rounds or 1 round; see text":            "1d4 раунда или 1 раунд, см. текст",
+	"2d4 rounds":                                 "2d4 раунда",
+	"1d6+2 rounds":                               "1d6+2 раунда",
+	"1 hour/level":                               "1 час/уровень",
+	"1 hour/level (D)":                           "1 час/уровень или до отмены",
+	"1 hour/level or until discharged; see text": "1 час/уровень или до отмены; см. текст",
+	"2 hours/level":                              "2 часа/уровень",
+	"until landing or 1 round/level":             "до приземл. или 1 раунд/ур.",
+	"1 day/level":                                "1 день/уровень",
 
 	//Range
-	"personal": "на себя",
-	"touch":    "касание",
+	"personal":                                    "на себя",
+	"touch":                                       "касание",
+	"personal or touch":                           "на себя или касание",
+	"personal or close (25 ft. + 5 ft./2 levels)": "на себя или близкая (25 фт. + 5 фт./2 уровня)",
+
 	"close (25 ft. + 5 ft./2 levels)": "близкая (25 фт. + 5 фт./2 уровня)",
 	"medium (100 ft. + 10 ft./level)": "средняя (100 фт. + 10 фт./уровнень)",
+	"medium (100 ft. + 10 ft. level)": "средняя (100 фт. + 10 фт./уровнень)",
+	"long (400 ft. + 40 ft./level)":   "дальняя (400 фт. + 40 фт./уровень)",
+
 	"10 ft.": "10 футов",
+	"15 ft.": "15 футов",
+	"20 ft.": "20 футов",
+	"30 ft.": "30 футов",
+	"50 ft.": "50 футов",
 	"60 ft.": "60 футов",
 
 	//Area
+	"cone-shaped emanation":                "коническая эманация",
+	"cone-shaped burst":                    "конический всплеск",
+	"50-ft.-radius burst, centered on you": "50 футов вокруг колдующего",
+
+	"20-ft.-radius emanation centered on a point in space":                    "20 футовая сфера с центром в заданной точке",
+	"one creature, one object, or a 5-ft. cube":                               "одно создание, один объект или 5 футовый куб",
+	"The caster and all allies within a 50-ft. burst, centered on the caster": "Колдующий и союзники в пределах 50 футов",
+	"one or more living creatures within a 10-ft.-radius burst":               "одно или несколько живых созданий внутри 10 футовой сферы",
+	"several living creatures, no two of which may be more than 30 ft. apart": "несколько живых созданий, никакие два из которых не стоят дальше 30 футов друг от друга",
 }
 
 func T(str string) string {
 	if res, ok := translation[str]; ok {
 		return res
 	} else {
+		if str != "" {
+			log.Println("untranslated:", str)
+		}
 		return str
 	}
 }
@@ -92,11 +145,21 @@ type Component struct {
 	Name, Image string
 }
 
+var (
+	COMP_VERBAL       = Component{"верб.", "verbal"}
+	COMP_SOMATIC      = Component{"сомат.", "somatic"}
+	COMP_MATERIAL     = Component{"матер.", "material"}
+	COMP_FOCUS        = Component{"фокус", "focus"}
+	COMP_DIVINE_FOCUS = Component{"бож.", "divine_focus"}
+	COMP_F_DF         = Component{"фокус/бож.", "f_df"}
+	COMP_M_DF         = Component{"матер./бож.", "m_df"}
+)
+
 type Spell struct {
 	Name, School, Effect      string
 	CastTime, Duration, Range string
 	Area, AreaImg             string
-	Descriptor                string
+	Descriptors               []string
 	Description               template.HTML
 	Level                     int
 	Components                []Component
@@ -133,12 +196,56 @@ func GetLevel(str string) (int, error) {
 	return strconv.Atoi(str)
 }
 
-func GetAreaImg(area string) string {
-	return ""
+func GetAreaImg(rng, area string) string {
+	switch {
+	case strings.HasPrefix(area, "cone-shaped"):
+		switch rng {
+		case "15 ft.":
+			return "cone-15"
+		case "30 ft.":
+			return "cone-30"
+		case "60 ft.":
+			return "cone-60"
+		default:
+			return ""
+		}
+	case strings.Contains(area, "5-ft.-radius"):
+		return "radius-5"
+	case strings.Contains(area, "10-ft.-radius"):
+		return "radius-10"
+	case strings.Contains(area, "20-ft.-radius"):
+		return "radius-20"
+	case strings.Contains(area, "30-ft.-radius"):
+		return "radius-30"
+	default:
+		return ""
+	}
 }
 
 func GetComponents(str string) []Component {
-	return nil
+	var comp []Component
+	for _, item := range strings.Split(str, ", ") {
+		item = strings.TrimSpace(item)
+		switch {
+		case strings.HasPrefix(item, "M/DF") || strings.HasPrefix(item, "DF/M"):
+			comp = append(comp, COMP_M_DF)
+		case strings.HasPrefix(item, "F/DF") || strings.HasPrefix(item, "DF/F"):
+			comp = append(comp, COMP_F_DF)
+		case strings.HasPrefix(item, "M"):
+			comp = append(comp, COMP_MATERIAL)
+		case strings.HasPrefix(item, "F"):
+			comp = append(comp, COMP_FOCUS)
+		case strings.HasPrefix(item, "V"):
+			comp = append(comp, COMP_VERBAL)
+		case strings.HasPrefix(item, "S"):
+			comp = append(comp, COMP_SOMATIC)
+		case strings.HasPrefix(item, "DF"):
+			comp = append(comp, COMP_DIVINE_FOCUS)
+		default:
+			//log.Println("Unknown component: ", item)
+		}
+	}
+	return comp
 }
 
 func GenerateCards(writer io.Writer, class string, level int) error {
@@ -194,16 +301,18 @@ func GenerateCards(writer io.Writer, class string, level int) error {
 		spell := Spell{
 			Name:        value("name_ru", "name"),
 			School:      value("school"),
-			Descriptor:  strings.Join(strings.Split(value("descriptor"), ", "), " "),
 			Effect:      value("effect_ru", "effect"),
 			CastTime:    T(value("casting_time")),
 			Duration:    T(value("duration")),
 			Range:       T(value("range")),
 			Area:        T(value("area")),
-			AreaImg:     GetAreaImg(value("area")),
+			AreaImg:     GetAreaImg(value("range"), value("area")),
 			Description: template.HTML(value("description_formated_ru", "description_formated")),
 			Level:       level,
 			Components:  GetComponents(value("components")),
+		}
+		if descStr := value("descriptor"); descStr != "" {
+			spell.Descriptors = strings.Split(descStr, ", ")
 		}
 		data.Spells = append(data.Spells, spell)
 	}
@@ -213,7 +322,12 @@ func GenerateCards(writer io.Writer, class string, level int) error {
 }
 
 func main() {
-	err := GenerateCards(os.Stdout, "wiz", 0)
+	var (
+		class = flag.String("class", "wiz", "Character class")
+		level = flag.Int("level", 0, "Spell level")
+	)
+	flag.Parse()
+	err := GenerateCards(os.Stdout, *class, *level)
 	if err != nil {
 		log.Fatalln(err)
 	}
